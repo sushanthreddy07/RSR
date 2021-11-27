88325d84f2dc2f69e77d7eb50ea18385f417f275
sushanthreddy07 <rachalasushanth007@gmail.com>
2021-11-27 19:22:10 +0530

    file added

---
+++ pythonProject2/alexa.py
@@ -0,0 +1,32 @@
+import speech_recognition as sr
+import pyttsx3
+listener=sr.Recognizer()
+engine=pyttsx3.init()
+voices=engine.getProperty('voices')
+engine.setProperty('voice',voices[1].id)
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+def take_command():
+
+    try:
+        with sr.Microphone() as source:
+            print('listening....')
+            voice=listener.listen(source)
+            command=listener.recognize_google(voice)
+            command=command.lower()
+            if 'alexa' in command:
+                 command=command.replace('alexa'," ")
+
+
+    except:
+        pass
+     return command
+def run_alexa():
+   command=take_command()
+      print(command)
+    if 'play' in command:
+        talk('playing')
+        print('playing')
+run_alexa()
+
